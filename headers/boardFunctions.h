a777f6af8a91487bb60a6b674dee96a2cb16d74c
todorkermedchiev <todorkerm@gmail.com>
2022-01-20 15:50:33 +0200

    Remove source files

--- headers/boardFunctions.h
+++ headers/boardFunctions.h
@@ -1,11 +1,108 @@
-#ifndef BOARDFUNCTIONS
-#define BOARDFUNCTIONS
+#ifndef __BOARDFUNCTIONS
+#define __BOARDFUNCTIONS
+#include <iostream>
 #include "constants.h"
-#include "../sources/boardFunctions.cpp"
 
-void fillBoard(char [][BOARD_SIZE], char);
-void fillPrintedBoard(char [][PRINTED_BOARD_COLS], char);
-void makeBorders(char [][PRINTED_BOARD_COLS]);
-void print(char [][BOARD_SIZE], char [][PRINTED_BOARD_COLS]);
+using namespace std;
 
-#endif
+void fillBoard(char board [][BOARD_SIZE], char character = ' ')
+{
+    for (int row = 0; row < BOARD_SIZE; ++row) {
+        for (int column = 0; column < BOARD_SIZE; ++column) {
+            board[row][column] = character;
+        }
+    }
+
+    // Filling the first row, the last row, the first column and the last column with the numbers from 1 to 9
+    for (int i = 1; i < BOARD_SIZE - 1; ++i) {
+        board[0][i] = i + '0';
+        board[i][0] = i + '0';
+        board[10][i] = i + '0';
+        board[i][10] = i + '0';
+    }
+}
+
+void fillPrintedBoard(char board[][PRINTED_BOARD_COLS], char character = ' ')
+{
+    for (int row = 0; row < PRINTED_BOARD_ROWS; ++row) {
+        for (int column = 0; column < PRINTED_BOARD_COLS; ++column) {
+            board[row][column] = character;
+        }
+    }
+}
+
+void makeBorders(char printedBoard[][PRINTED_BOARD_COLS])
+{
+    fillPrintedBoard(printedBoard);
+
+    for (int row = 0; row < PRINTED_BOARD_ROWS; ++row) {
+        for (int column = 0; column < PRINTED_BOARD_COLS; ++column) {
+            // Adding horizontal lines
+            if (row % 2 == 0) {
+                printedBoard[row][column] = (char) -51; // '═'
+            }
+
+            // Adding vertical lines
+            if (column % 4 == 0) {
+                printedBoard[row][column] = (char) -70; // '║'
+            }
+
+            if (row % 2 == 0 && column % 4 == 0) {
+                // Adding corner characters between the cells
+                printedBoard[row][column] = (char) -50; // '╬'
+
+                // Adding corner characters on the first row between the cells
+                if (row == 0) {
+                    printedBoard[row][column] = (char) -53; // '╦'
+                }
+
+                // Adding corner characters on the last row between the cells
+                if (row == PRINTED_BOARD_ROWS - 1) {
+                    printedBoard[row][column] = (char) -54; // '╩'
+                }
+
+                // Adding corner characters on the first column between the cells
+                if (column == 0) {
+                    printedBoard[row][column] = (char) -52; // '╠'
+                }
+
+                // Adding corner characters on the last column between the cells
+                if (column == PRINTED_BOARD_COLS - 1) {
+                    printedBoard[row][column] = (char) -71; // '╣'
+                }
+            }
+        }
+    }
+
+    // Adding corner characters
+    printedBoard[0][0] = (char) -55; // '╔'
+    printedBoard[0][PRINTED_BOARD_COLS - 1] = (char) -69; // '╗'
+    printedBoard[PRINTED_BOARD_ROWS - 1][0] = (char) -56; // '╚'
+    printedBoard[PRINTED_BOARD_ROWS - 1][PRINTED_BOARD_COLS - 1] = (char) -68; // '╝'
+
+    // Used character set:
+    // -69 = '╗'; -68 = '╝'; -56 = '╚'; -55 = '╔'; -51 = '═'; -70 = '║';
+    // -50 = '╬'; -53 = '╦'; -54 = '╩'; -52 = '╠'; -71 = '╣';
+}
+
+void print(char board[][BOARD_SIZE], char printedBoard[][PRINTED_BOARD_COLS])
+{
+    // Adding elements from the board to the printed board
+    for (int row = 0; row < BOARD_SIZE; ++row) {
+        for (int column = 0; column < BOARD_SIZE; ++column) {
+            printedBoard[row * 2 + 1][column * 4 + 2] = board[row][column];
+        }
+    }
+
+    // Printing "printedBoard"
+    cout << endl;
+    for (int i = 0; i < PRINTED_BOARD_ROWS; ++i) {
+        for (int j = 0; j < PRINTED_BOARD_COLS; ++j) {
+            cout << printedBoard[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+#endif // __BOARDFUNCTIONS
--- headers/generateMines.h
+++ headers/generateMines.h
@@ -1,8 +1,45 @@
-#ifndef GENERATEMINES
-#define GENERATEMINES
+#ifndef __GENERATEMINES
+#define __GENERATEMINES
 #include "constants.h"
-#include "../sources/generateMines.cpp"
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <time.h>
 
-void generateMinesBoard(bool minesBoard[][BOARD_SIZE]);
+using namespace std;
 
-#endif
+void generateMinesBoard(bool minesBoard[][BOARD_SIZE], int minesCoordinates[MINES_COUNT] = {})
+{
+    bool isValidPosition = true;
+    unsigned int position = 0;
+    unsigned int index = 0;
+    int row = 0;
+    int column = 0;
+    srand(time(NULL));
+
+    while (index < MINES_COUNT) {
+        position = rand() % 100; // Generating random number between 0 and 99;
+        // This number indicates the coordinates of the mine
+        // The first digit is the row and the second is the column, so the number must be between 11 and 99
+        isValidPosition = position >= 11 && position <= 99 && position % 10 != 0;
+
+        for (int j = 0; j < MINES_COUNT && isValidPosition; ++j) {
+            if (position == minesCoordinates[j]) {
+                // There is a repeating number
+                isValidPosition = false;
+            }
+        }
+
+        if (isValidPosition) {
+            minesCoordinates[index] = position;
+            ++index;
+        }
+    }
+
+    for (int i = 0; i < MINES_COUNT; ++i) {
+        row = minesCoordinates[i] / 10;
+        column = minesCoordinates[i] % 10;
+        minesBoard[row][column] = true;
+    }
+}
+#endif // __GENERATEMINES
